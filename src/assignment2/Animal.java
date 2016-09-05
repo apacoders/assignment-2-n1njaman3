0bd6ba0a4ad37d8cb31ffa5a507a8d81c8d1c739
tim <tim@192.168.150.196>
2016-09-05 10:46:02 -0600

    Added comment

---
+++ src/assignment2/Animal.java
@@ -0,0 +1,18 @@
+package assignment2;
+
+/**
+ * REQUIREMENT 3:
+ * 
+ * The class must have an immutable String called speakParameter (note camel case)
+ * 
+ * REQUIREMENT 4:
+ * 
+ * The class must have a non-static method called speak which prints the speakParameter
+ * onto the screen.
+ * 
+ * 
+ * @author tim
+ */
+public class Animal {
+    
+}
---
+++ src/assignment2/Assignment2.java
@@ -0,0 +1,40 @@
+package assignment2;
+
+/**
+ * Instructions for this assignment.  Download this template from GitHub, fill in
+ * the author comment below with your name and implement the requirements below.
+ * 
+ * If code is turned in and cannot be compiled, you will receive 0 points.
+ * If a requirement is not implemented, you will receive 0 points.
+ * Requirements are worth 50 points each.
+ * 
+ * @author myname
+ * 
+ * Requirements for main method
+ * 
+ * REQUIREMENT 1:
+ * 
+ * Create a ListArray of animal objects and place in that array animal objects with
+ * the following specifications:
+ * 
+ * an Animal object that goes "Woof" when the speak method is called (see the Animal class for details)
+ * an Animal object that goes "Meow" when the speak method is called
+ * an Animal object that goes "Narly" when the speak method is called
+ * an Animal object that goes "Squeek" when the speak method is called
+ * 
+ * REQUIREMENT 2:
+ * 
+ * In a loop, call all the speak methods on the animal objects in the ArrayList.
+ * The loop must be written such that if I add another animal object to the ArrayList
+ * then the loop does not have to be updated to call the speak method.
+ * 
+ * 
+ */
+public class Assignment2 {
+
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
